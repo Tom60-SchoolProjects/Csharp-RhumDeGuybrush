d611d85ece893633251c1a428b4d931f881af07e
LimpalaerCyril <83960534+LimpalaerCyril@users.noreply.github.com>
2021-06-10 20:17:45 +0200

    Update Parcelle.cs

--- Rhum de Guybrush/Parcelle.cs
+++ Rhum de Guybrush/Parcelle.cs
@@ -9,6 +9,9 @@ namespace Rhum_de_Guybrush
     public class Parcelle
     {
         #region Enumerations
+        /// <summary>
+        /// Permettre d'identifier le type d'une parcelle.
+        /// </summary>
         public enum TypeParcelle
         {
             Normal,
@@ -18,17 +21,34 @@ namespace Rhum_de_Guybrush
         #endregion
 
         #region Attributs
+        /// <summary>
+        /// Variables locales utiles à la création et modification d'une parcelle.
+        /// </summary>
+        // id => c
         private TypeParcelle type;
+        // type => Normal
         private List<Unite> unites;
+        /*
+         * (0,0) => Vrai, (0,1) => Vrai, (0,2) => Vrai
+         * (1,0) => Vrai, (1,1) => Vrai, (1,2) => Faux
+         * (2,0) => Vrai, (2,1) => Vrai, (2,2) => Vrai
+         * 3 x 3
+         * */
         #endregion
 
         #region Accesseur
+        /// <summary>
+        /// Permettre l'accès a des variables locales.
+        /// </summary>
         public TypeParcelle Type => type;
         public IReadOnlyList<Unite> Unites => unites;
         public int Taille => Unites.Count;
         #endregion
 
         #region Constructeur
+        /// <summary>
+        /// Permettre d'instancier une parcelle de 2 façons différentes.
+        /// </summary>
         public Parcelle(TypeParcelle type, List<Unite> unites)
         {
             this.type = type;
@@ -42,6 +62,9 @@ namespace Rhum_de_Guybrush
         #endregion
 
         #region Méthodes
+        /// <summary>
+        /// Permettre d'ajouter ou supprimer simplement un objet de type Unite.
+        /// </summary>
         public void Ajouter(Unite unite) => unites.Add(unite);
         public void Supprimer(int index) => unites.RemoveAt(index);
         #endregion
