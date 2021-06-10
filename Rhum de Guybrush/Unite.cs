0e13ac145992eb2357dffb0b2b957c0c264eca1f
LimpalaerCyril <83960534+LimpalaerCyril@users.noreply.github.com>
2021-06-10 20:18:09 +0200

    Update Unite.cs

--- Rhum de Guybrush/Unite.cs
+++ Rhum de Guybrush/Unite.cs
@@ -1,18 +1,27 @@
-﻿namespace Rhum_de_Guybrush
+namespace Rhum_de_Guybrush
 {
     public class Unite
     {
         #region Attributs
+        /// <summary>
+        /// Variables locales utiles à la création et au repérage d'une Unitée.
+        /// </summary>
         private int x;
         private int y;
         #endregion
 
         #region Accesseur
+        /// <summary>
+        /// Permettre d'accéder aux variables locales de la classe Unite.
+        /// </summary>
         public int X => x;
         public int Y => y;
         #endregion
 
         #region Constructeur
+        /// <summary>
+        /// Permettre d'instancier un objet de type Unite.
+        /// </summary>
         public Unite(int x, int y)
         {
             this.x = x;
